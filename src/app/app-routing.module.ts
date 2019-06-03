4ebdfeec7467463976cbf8daf2d3599d278a7c83
DrStupit <kerevreddy@hotmail.com>
2019-06-03 18:55:47 +0200

    add angular material to project

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { LoginComponent } from './login/login.component';
+import { RegistrationComponent } from './registration/registration.component';
+import { ActivityComponent } from './activity/activity.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'login', component: LoginComponent },
+  { path: 'register', component: RegistrationComponent },
+  { path: 'activity', component: ActivityComponent },
+  { path: '', redirectTo: '/register', pathMatch: 'full' }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
